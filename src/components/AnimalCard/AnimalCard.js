0dcdb903fdf0e222579707dcf611d170a6e840e1
haihung <hungnh12062000@gmail.com>
2023-02-05 16:36:24 +0700

    How To Create Wrapper Components in React with Props

--- src/components/AnimalCard/AnimalCard.js
+++ src/components/AnimalCard/AnimalCard.js
@@ -1,43 +1,49 @@
 import React from 'react';
 
 import PropTypes from 'prop-types';
+import './AnimalCard.css';
 
-import './AnimalCard.css'
+import AnimalDetails from '../AnimalDetails/AnimalDetails';
+import Card from '../Card/Card';
 
 export default function AnimalCard({
-     additional,
-     diet,
      name,
-     scientificName,
-     showAdditional,
-     size
+     size,
+     ...props
+     // additional,
+     // diet,
+     // scientificName,
+     // showAdditional,
 }) {
 
      return (
           <div className="animal-wrapper">
-               <h2>{name}</h2>
-               <h3>{scientificName}</h3>
-               <h4>{size}kg</h4>
-               <div>{diet.join(', ')}.</div>
-               <button onClick={() => showAdditional(additional)}>More Info</button>
+               {/* <button onClick={() => showAdditional(additional)}>More Info</button> */}
+
+               <Card title="Animal" details={<AnimalDetails
+                    {...props}
+               />}>
+                    <h3>{name}</h3>
+                    <div>{size}kg</div>
+               </Card>
           </div>
      );
 }
 
 AnimalCard.propTypes = {
-     additional: PropTypes.shape({
-          link: PropTypes.string,
-          notes: PropTypes.string
-     }),
-     diet: PropTypes.arrayOf(PropTypes.string).isRequired,
      name: PropTypes.string.isRequired,
-     scientificName: PropTypes.string.isRequired,
-     showAdditional: PropTypes.func.isRequired,
      size: PropTypes.number.isRequired,
+     // additional: PropTypes.shape({
+     //      link: PropTypes.string,
+     //      notes: PropTypes.string
+     // }),
+     // diet: PropTypes.arrayOf(PropTypes.string).isRequired,
+     // scientificName: PropTypes.string.isRequired,
+     // showAdditional: PropTypes.func.isRequired,
 }
 
-AnimalCard.defaultProps = {
-     additional: {
-          notes: 'No Additional Information'
-     }
-}
+// AnimalCard.defaultProps = {
+//      additional: {
+//           notes: 'No Additional Information'
+//      }
+// }
